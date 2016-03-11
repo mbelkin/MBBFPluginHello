9e38113e72062fedaede7364fdd7a46784c29c8c
mbelkin <michael@belkininteractive.com>
2016-03-10 16:27:18 -0800

    using command delegate

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -4,16 +4,16 @@
 
 - (void)greet:(CDVInvokedUrlCommand*)command
 {
-
-    NSString* callbackId = [command callbackId];
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
-
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-
-    [self success:result callbackId:callbackId];
+	
+	NSString* callbackId = [command callbackId];
+	NSString* name = [[command arguments] objectAtIndex:0];
+	NSString* msg = [NSString stringWithFormat: @"Hellooo, %@", name];
+	
+	CDVPluginResult* result = [CDVPluginResult
+							   resultWithStatus:CDVCommandStatus_OK
+							   messageAsString:msg];
+	
+	[self.commandDelegate sendPluginResult:result callbackId:callbackId];
 }
 
 @end
